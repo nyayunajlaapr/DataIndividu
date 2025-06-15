2c5770a2cfb464eb9e379bcfbe7db81765e9825b
Fathya Ariyani <fathyariyani@gmail.com>
2025-06-15 16:56:00 +0700

    edit schema & resolver

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -8,7 +8,57 @@ const resolvers = {
       ];
       return data.find((p) => p.id === id);
     },
+    getAllPasien: () => {
+      // Data dummy atau bisa juga dari database kalau udah ada
+      const data = [
+        { id: 1, nama: "Andi", tanggal_lahir: "2000-01-01"},
+        { id: 2, nama: "Budi", tanggal_lahir: "1999-05-12"},
+        { id: 3, nama: "Citra", tanggal_lahir: "2002-07-20"},
+      ];
+      return data;
+    },
   },
 };
 
 module.exports = resolvers;
+
+// const tenagaKesehatanModel = require('../modeljs/tenagakesehatan');
+// const pasienModel = require('../modeljs/pasien');
+// const dokterModel = require('../modeljs/dokter');
+// const perawatModel = require('../modeljs/perawat');
+
+// const resolvers = {
+//   Query: {
+//     tenagaKesehatan: (_, { id_nakes }) => tenagaKesehatanModel.getById(id_nakes),
+//     allTenagaKesehatan: () => tenagaKesehatanModel.getAll(),
+
+//     pasien: (_, { id_pasien }) => pasienModel.getById(id_pasien),
+//     allPasien: () => pasienModel.getAll(),
+
+//     dokter: (_, { id_dokter }) => dokterModel.getById(id_dokter),
+//     allDokter: () => dokterModel.getAll(),
+
+//     perawat: (_, { id_perawat }) => perawatModel.getById(id_perawat),
+//     allPerawat: () => perawatModel.getAll(),
+//   },
+
+//   Mutation: {
+//     createTenagaKesehatan: (_, args) => tenagaKesehatanModel.create(args),
+//     updateTenagaKesehatan: (_, { id_nakes, ...rest }) => tenagaKesehatanModel.update(id_nakes, rest),
+//     deleteTenagaKesehatan: (_, { id_nakes }) => tenagaKesehatanModel.remove(id_nakes),
+
+//     createPasien: (_, args) => pasienModel.create(args),
+//     updatePasien: (_, { id_pasien, ...rest }) => pasienModel.update(id_pasien, rest),
+//     deletePasien: (_, { id_pasien }) => pasienModel.remove(id_pasien),
+
+//     createDokter: (_, args) => dokterModel.create(args),
+//     updateDokter: (_, { id_dokter, ...rest }) => dokterModel.update(id_dokter, rest),
+//     deleteDokter: (_, { id_dokter }) => dokterModel.remove(id_dokter),
+
+//     createPerawat: (_, args) => perawatModel.create(args),
+//     updatePerawat: (_, { id_perawat, ...rest }) => perawatModel.update(id_perawat, rest),
+//     deletePerawat: (_, { id_perawat }) => perawatModel.remove(id_perawat),
+//   }
+// };
+
+// module.exports = resolvers;
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -9,7 +9,87 @@ const typeDefs = gql`
 
   type Query {
     pasien(id: Int!): Pasien
+    getAllPasien: [Pasien]
   }
 `;
 
 module.exports = typeDefs;
+
+// const { gql } = require('apollo-server-express');
+
+// const typeDefs = gql`
+//   type TenagaKesehatan {
+//     id_nakes: Int
+//     nip: String
+//     role: String
+//     str: String
+//   }
+
+//   type Pasien {
+//     id_pasien: Int
+//     nik: String
+//     nama_lengkap: String
+//     jenis_kelamin: String
+//     tanggal_lahir: String
+//     no_bpjs: String
+//     pekerjaan: String
+//     status_pernikahan: String
+//   }
+
+//   type Dokter {
+//     id_dokter: Int
+//     id_nakes: Int
+//     nip: String
+//     nama_dokter: String
+//     status: String
+//     str: String
+//     spesialisasi: String
+//     no_hp: String
+//   }
+
+//   type Perawat {
+//     id_perawat: Int
+//     id_nakes: Int
+//     nip: String
+//     nama_perawat: String
+//     status: String
+//     str: String
+//     spesialisasi: String
+//     no_hp: String
+//   }
+
+//   type Query {
+//     tenagaKesehatan(id_nakes: Int!): TenagaKesehatan
+//     allTenagaKesehatan: [TenagaKesehatan]
+
+//     pasien(id_pasien: Int!): Pasien
+//     allPasien: [Pasien]
+
+//     dokter(id_dokter: Int!): Dokter
+//     allDokter: [Dokter]
+
+//     perawat(id_perawat: Int!): Perawat
+//     allPerawat: [Perawat]
+//   }
+
+//   type Mutation {
+//     createTenagaKesehatan(nip: String!, role: String!, str: String): TenagaKesehatan
+//     updateTenagaKesehatan(id_nakes: Int!, nip: String, role: String, str: String): TenagaKesehatan
+//     deleteTenagaKesehatan(id_nakes: Int!): Boolean
+
+//     createPasien(nik: String!, nama_lengkap: String!, jenis_kelamin: String, tanggal_lahir: String, no_bpjs: String, pekerjaan: String, status_pernikahan: String): Pasien
+//     updatePasien(id_pasien: Int!, nik: String, nama_lengkap: String, jenis_kelamin: String, tanggal_lahir: String, no_bpjs: String, pekerjaan: String, status_pernikahan: String): Pasien
+//     deletePasien(id_pasien: Int!): Boolean
+
+//     createDokter(idNakes: Int, nip: String, namaDokter: String!, status: String, str: String, spesialisasi: String, noHp: String): Dokter
+//     updateDokter(id_dokter: Int!, idNakes: Int, nip: String, namaDokter: String, status: String, str: String, spesialisasi: String, noHp: String): Dokter
+//     deleteDokter(id_dokter: Int!): Boolean
+
+//     createPerawat(idNakes: Int, nip: String, namaPerawat: String!, status: String, str: String, spesialisasi: String, noHp: String): Perawat
+//     updatePerawat(id_perawat: Int!, idNakes: Int, nip: String, namaPerawat: String, status: String, str: String, spesialisasi: String, noHp: String): Perawat
+//     deletePerawat(id_perawat: Int!): Boolean
+//   }
+// `;
+
+// module.exports = typeDefs;
+
